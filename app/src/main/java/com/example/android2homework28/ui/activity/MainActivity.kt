d56a78eb6fdf8375dc46a2ce11e14c47f7c0f932
LENOVO <test23062000@gmail.com>
2024-06-21 22:52:06 +0300

    Lesson2

--- app/src/main/java/com/example/android2homework28/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/android2homework28/ui/activity/MainActivity.kt
@@ -1,10 +1,7 @@
-package com.example.android2homework28
+package com.example.android2homework28.ui.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.android2homework28.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/example/android2homework28/ui/fragments/note/NoteFragment.kt
@@ -0,0 +1,32 @@
+package com.example.android2homework28.ui.fragments.note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android2homework28.R
+import com.example.android2homework28.databinding.FragmentNoteBinding
+
+class NoteFragment : Fragment() {
+
+    private var _binding: FragmentNoteBinding? = null
+    private val binding: FragmentNoteBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
